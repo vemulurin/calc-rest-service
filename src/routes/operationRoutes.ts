8f4d5f998d5eee96e7b8d82b5e8cf8d524ec0a59
vemulurin <vemulurin@noreply.github.com>
2020-04-21 20:11:58 +1000

    added new endpoints

--- src/routes/operationRoutes.ts
+++ src/routes/operationRoutes.ts
@@ -1,20 +1,75 @@
-import {Request, Response, NextFunction} from "express";
+import { Request, Response, NextFunction } from "express";
 //import { ContactController } from "../controllers/crmController";
 
-export class Routes { 
-    
+export class Routes {
+
     //public contactController: ContactController = new ContactController() 
-    
-    public routes(app): void {   
-        
+
+    public routes(app): void {
+
         app.route('/')
-        .get((req: Request, res: Response) => {            
-            res.status(200).send({
-                message: 'GET request successfulll!!!!'
+            .get((req: Request, res: Response) => {
+                res.status(200).send({
+                    message: 'Success!'
+                })
             })
-        })
-        
 
-
+        //GET - Addition
+        app.route('/add')
+            .get((req: Request, res: Response) => {
+                try {
+                    const num1 = +req.query.num1;
+                    const num2 = +req.query.num2;
+                    res.status(200).send({ result: num1 + num2 });
+                }
+                catch (error) {
+                    res.send({
+                        message: error
+                    })
+                }
+            });
+        //GET - Subtraction
+        app.route('/subtract')
+            .get((req: Request, res: Response) => {
+                try {
+                    const num1 = +req.query.num1;
+                    const num2 = +req.query.num2;
+                    res.status(200).send({ result: num1 - num2 });
+                }
+                catch (error) {
+                    res.send({
+                        message: error
+                    })
+                }
+            });
+        //GET - Multiplication
+        app.route('/multiply')
+            .get((req: Request, res: Response) => {
+                try {
+                    const num1 = +req.query.num1;
+                    const num2 = +req.query.num2;
+                    res.status(200).send({ result: num1 - num2 });
+                }
+                catch (error) {
+                    res.send({
+                        message: error
+                    })
+                }
+            });
+          //GET - Division
+          app.route('/divide')
+          .get((req: Request, res: Response) => {
+              try {
+                  const num1 = +req.query.num1;
+                  const num2 = +req.query.num2;
+                  res.status(200).send({ result: num1 / num2 });
+              }
+              catch (error) {
+                  res.send({
+                      message: error
+                  })
+              }
+          });
     }
+
 }
